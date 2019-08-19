083498dc05b3fd798d5dfe78c38f60a914314bcb
Rinnegatamante <rinnegatamante@gmail.com>
2019-08-19 14:21:08 +0200

    Using math-neon for some math operations.

--- source/utils/math_utils.c
+++ source/utils/math_utils.c
@@ -5,6 +5,7 @@
 
 #include "../shared.h"
 #include <math.h>
+#include <math_neon.h>
 
 // NOTE: matrices are row-major.
 
@@ -21,51 +22,43 @@ void matrix4x4_copy(matrix4x4 dst, const matrix4x4 src) {
 }
 
 void matrix4x4_multiply(matrix4x4 dst, const matrix4x4 src1, const matrix4x4 src2) {
-	int i, j, k;
-
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 4; j++) {
-			dst[i][j] = 0.0f;
-			for (k = 0; k < 4; k++)
-				dst[i][j] += src1[i][k] * src2[k][j];
-		}
-	}
+	matmul4_neon((float*)src2, (float*)src1, (float*)dst);
 }
 
 void matrix4x4_init_rotation_x(matrix4x4 m, float rad) {
-	float c = cosf(rad);
-	float s = sinf(rad);
-
+	float cs[2];
+	sincosf_c(rad, cs);
+	
 	matrix4x4_identity(m);
 
-	m[1][1] = c;
-	m[1][2] = -s;
-	m[2][1] = s;
-	m[2][2] = c;
+	m[1][1] = cs[1];
+	m[1][2] = -cs[0];
+	m[2][1] = cs[0];
+	m[2][2] = cs[1];
 }
 
 void matrix4x4_init_rotation_y(matrix4x4 m, float rad) {
-	float c = cosf(rad);
-	float s = sinf(rad);
+	float cs[2];
+	sincosf_c(rad, cs);
 
 	matrix4x4_identity(m);
 
-	m[0][0] = c;
-	m[0][2] = s;
-	m[2][0] = -s;
-	m[2][2] = c;
+	m[0][0] = cs[1];
+	m[0][2] = cs[0];
+	m[2][0] = -cs[0];
+	m[2][2] = cs[1];
 }
 
 void matrix4x4_init_rotation_z(matrix4x4 m, float rad) {
-	float c = cosf(rad);
-	float s = sinf(rad);
+	float cs[2];
+	sincosf_c(rad, cs);
 
 	matrix4x4_identity(m);
 
-	m[0][0] = c;
-	m[0][1] = -s;
-	m[1][0] = s;
-	m[1][1] = c;
+	m[0][0] = cs[1];
+	m[0][1] = -cs[0];
+	m[1][0] = cs[0];
+	m[1][1] = cs[1];
 }
 
 void matrix4x4_rotate_x(matrix4x4 m, float rad) {
@@ -178,7 +171,7 @@ void matrix4x4_init_frustum(matrix4x4 m, float left, float right, float bottom, 
 }
 
 void matrix4x4_init_perspective(matrix4x4 m, float fov, float aspect, float near, float far) {
-	float half_height = near * tanf(DEG_TO_RAD(fov) * 0.5f);
+	float half_height = near * tanf_neon(DEG_TO_RAD(fov) * 0.5f);
 	float half_width = half_height * aspect;
 
 	matrix4x4_init_frustum(m, -half_width, half_width, -half_height, half_height, near, far);
@@ -190,35 +183,20 @@ int matrix4x4_invert(matrix4x4 out, const matrix4x4 m) {
 	matrix4x4 inv;
 
 	inv[0][0] = m[1][1] * m[2][2] * m[3][3] - m[1][1] * m[3][2] * m[2][3] - m[1][2] * m[2][1] * m[3][3] + m[1][2] * m[3][1] * m[2][3] + m[1][3] * m[2][1] * m[3][2] - m[1][3] * m[3][1] * m[2][2];
-
 	inv[0][1] = -m[0][1] * m[2][2] * m[3][3] + m[0][1] * m[3][2] * m[2][3] + m[0][2] * m[2][1] * m[3][3] - m[0][2] * m[3][1] * m[2][3] - m[0][3] * m[2][1] * m[3][2] + m[0][3] * m[3][1] * m[2][2];
-
 	inv[0][2] = m[0][1] * m[1][2] * m[3][3] - m[0][1] * m[3][2] * m[1][3] - m[0][2] * m[1][1] * m[3][3] + m[0][2] * m[3][1] * m[1][3] + m[0][3] * m[1][1] * m[3][2] - m[0][3] * m[3][1] * m[1][2];
-
 	inv[0][3] = -m[0][1] * m[1][2] * m[2][3] + m[0][1] * m[2][2] * m[1][3] + m[0][2] * m[1][1] * m[2][3] - m[0][2] * m[2][1] * m[1][3] - m[0][3] * m[1][1] * m[2][2] + m[0][3] * m[2][1] * m[1][2];
-
 	inv[1][0] = -m[1][0] * m[2][2] * m[3][3] + m[1][0] * m[3][2] * m[2][3] + m[1][2] * m[2][0] * m[3][3] - m[1][2] * m[3][0] * m[2][3] - m[1][3] * m[2][0] * m[3][2] + m[1][3] * m[3][0] * m[2][2];
-
 	inv[1][1] = m[0][0] * m[2][2] * m[3][3] - m[0][0] * m[3][2] * m[2][3] - m[0][2] * m[2][0] * m[3][3] + m[0][2] * m[3][0] * m[2][3] + m[0][3] * m[2][0] * m[3][2] - m[0][3] * m[3][0] * m[2][2];
-
 	inv[1][2] = -m[0][0] * m[1][2] * m[3][3] + m[0][0] * m[3][2] * m[1][3] + m[0][2] * m[1][0] * m[3][3] - m[0][2] * m[3][0] * m[1][3] - m[0][3] * m[1][0] * m[3][2] + m[0][3] * m[3][0] * m[1][2];
-
 	inv[1][3] = m[0][0] * m[1][2] * m[2][3] - m[0][0] * m[2][2] * m[1][3] - m[0][2] * m[1][0] * m[2][3] + m[0][2] * m[2][0] * m[1][3] + m[0][3] * m[1][0] * m[2][2] - m[0][3] * m[2][0] * m[1][2];
-
 	inv[2][0] = m[1][0] * m[2][1] * m[3][3] - m[1][0] * m[3][1] * m[2][3] - m[1][1] * m[2][0] * m[3][3] + m[1][1] * m[3][0] * m[2][3] + m[1][3] * m[2][0] * m[3][1] - m[1][3] * m[3][0] * m[2][1];
-
 	inv[2][1] = -m[0][0] * m[2][1] * m[3][3] + m[0][0] * m[3][1] * m[2][3] + m[0][1] * m[2][0] * m[3][3] - m[0][1] * m[3][0] * m[2][3] - m[0][3] * m[2][0] * m[3][1] + m[0][3] * m[3][0] * m[2][1];
-
 	inv[2][2] = m[0][0] * m[1][1] * m[3][3] - m[0][0] * m[3][1] * m[1][3] - m[0][1] * m[1][0] * m[3][3] + m[0][1] * m[3][0] * m[1][3] + m[0][3] * m[1][0] * m[3][1] - m[0][3] * m[3][0] * m[1][1];
-
 	inv[2][3] = -m[0][0] * m[1][1] * m[2][3] + m[0][0] * m[2][1] * m[1][3] + m[0][1] * m[1][0] * m[2][3] - m[0][1] * m[2][0] * m[1][3] - m[0][3] * m[1][0] * m[2][1] + m[0][3] * m[2][0] * m[1][1];
-
 	inv[3][0] = -m[1][0] * m[2][1] * m[3][2] + m[1][0] * m[3][1] * m[2][2] + m[1][1] * m[2][0] * m[3][2] - m[1][1] * m[3][0] * m[2][2] - m[1][2] * m[2][0] * m[3][1] + m[1][2] * m[3][0] * m[2][1];
-
 	inv[3][1] = m[0][0] * m[2][1] * m[3][2] - m[0][0] * m[3][1] * m[2][2] - m[0][1] * m[2][0] * m[3][2] + m[0][1] * m[3][0] * m[2][2] + m[0][2] * m[2][0] * m[3][1] - m[0][2] * m[3][0] * m[2][1];
-
 	inv[3][2] = -m[0][0] * m[1][1] * m[3][2] + m[0][0] * m[3][1] * m[1][2] + m[0][1] * m[1][0] * m[3][2] - m[0][1] * m[3][0] * m[1][2] - m[0][2] * m[1][0] * m[3][1] + m[0][2] * m[3][0] * m[1][1];
-
 	inv[3][3] = m[0][0] * m[1][1] * m[2][2] - m[0][0] * m[2][1] * m[1][2] - m[0][1] * m[1][0] * m[2][2] + m[0][1] * m[2][0] * m[1][2] + m[0][2] * m[1][0] * m[2][1] - m[0][2] * m[2][0] * m[1][1];
 
 	det = m[0][0] * inv[0][0] + m[1][0] * inv[0][1] + m[2][0] * inv[0][2] + m[3][0] * inv[0][3];
