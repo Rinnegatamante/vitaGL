42352f032bc556995c3b128d5d10ff528a24914a
Rinnegatamante <rinnegatamante@gmail.com>
2021-01-04 21:34:10 +0100

    Added error handling to glFramebufferTexture.

--- source/framebuffers.c
+++ source/framebuffers.c
@@ -140,6 +140,12 @@ void glFramebufferTexture(GLenum target, GLenum attachment, GLuint tex_id, GLint
 		SET_GL_ERROR(GL_INVALID_ENUM)
 		break;
 	}
+	
+#ifndef SKIP_ERROR_HANDLING
+	if (!fb) {
+		SET_GL_ERROR(GL_INVALID_OPERATION)
+	}
+#endif
 
 	// Aliasing to make code more readable
 	texture *tex = &texture_slots[tex_id];
