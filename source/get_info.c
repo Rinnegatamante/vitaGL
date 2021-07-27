2c2c55f4cb3b108ce7986e03a04511910f1be950
Rinnegatamante <rinnegatamante@gmail.com>
2021-07-27 21:03:33 +0200

    Added GL_EXT_texture_env_add extension support.

--- source/get_info.c
+++ source/get_info.c
@@ -25,7 +25,7 @@
 
 #include "shared.h"
 
-#define NUM_EXTENSIONS 12 // Number of supported extensions
+#define NUM_EXTENSIONS 13 // Number of supported extensions
 
 static GLubyte *extensions[NUM_EXTENSIONS] = {
 	"GL_OES_vertex_half_float",
@@ -39,7 +39,8 @@ static GLubyte *extensions[NUM_EXTENSIONS] = {
 	"GL_EXT_texture_compression_dxt5",
 	"GL_EXT_texture_compression_s3tc",
 	"GL_IMG_texture_compression_pvrtc",
-	"GL_WIN_phong_shading"
+	"GL_WIN_phong_shading",
+	"GL_EXT_texture_env_add"
 };
 static GLubyte *extension = NULL;
 
