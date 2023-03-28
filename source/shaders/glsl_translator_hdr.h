cb04039bf8264bf6b7de4118fbce919aa1d2261a
Rinnegatamante <rinnegatamante@gmail.com>
2023-03-28 16:01:13 +0200

    Added inversesqrt and greaterThanEqual support to GLSL translator.

--- source/shaders/glsl_translator_hdr.h
+++ source/shaders/glsl_translator_hdr.h
@@ -29,7 +29,9 @@ inline int vglMul(int v1, int v2) { return v1 * v2; }
 inline int4 vglMul(int4 v1, int v2) { return v1 * v2; }
 inline int3 vglMul(int3 v1, int v2) { return v1 * v2; }
 inline int2 vglMul(int2 v1, int v2) { return v1 * v2; }
+#define greaterThanEqual(x, y) (x >= y)
 #define texture2DProj(x,y) tex2Dproj(x,y)
+#define inversesqrt rsqrt
 #define samplerCube samplerCUBE
 inline float4 glslTexture2D(sampler2D x, float2 s) { return tex2D(x,s); }
 inline float4 glslTexture2D(sampler2D x, float3 s) { return tex2D(x,s); }
