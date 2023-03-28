f28e88dfaff454e7927f06ed18a6c06a8b3bd4e2
Andy Nguyen <theofficialflow1996@gmail.com>
2023-03-29 00:00:15 +0200

    Make gpu_store_texture_data inlineable.

--- source/utils/gpu_utils.c
+++ source/utils/gpu_utils.c
@@ -301,35 +301,6 @@ int tex_format_to_alignment(SceGxmTextureFormat format) {
 	}
 }
 
-void gpu_store_texture_data(uint32_t orig_w, uint32_t w, uint32_t h, uint32_t src_stride, const void *src_data, void *dst_data, uint8_t src_bpp, uint8_t bpp, uint32_t (*read_cb)(void *), void (*write_cb)(void *, uint32_t), GLboolean fast_store, GLint xoffset) {
-	uint32_t dst_stride = ALIGN(orig_w, 8) * bpp;
-	uint8_t *src;
-	uint8_t *dst;
-	int i, j;
-	if (fast_store) { // Internal Format and Data Format are the same, we can just use vgl_fast_memcpy for better performance
-		if (xoffset == 0 && src_stride == dst_stride && src_stride == orig_w * bpp) // Texture size is already aligned, we can use a single vgl_fast_memcpy for better performance
-			vgl_fast_memcpy(dst_data, src_data, w * h * bpp);
-		else {
-			for (i = 0; i < h; i++) {
-				src = (uint8_t *)src_data + src_stride * i;
-				dst = (uint8_t *)dst_data + dst_stride * i;
-				vgl_fast_memcpy(dst, src, w * bpp);
-			}
-		}
-	} else { // Different internal and data formats, we need to go with slower callbacks system
-		for (i = 0; i < h; i++) {
-			src = (uint8_t *)src_data + src_stride * i;
-			dst = (uint8_t *)dst_data + dst_stride * i;
-			for (j = 0; j < w; j++) {
-				uint32_t clr = read_cb(src);
-				write_cb(dst, clr);
-				src += src_bpp;
-				dst += bpp;
-			}
-		}
-	}
-}
-
 void *gpu_alloc_palette(const void *data, uint32_t w, uint32_t bpe) {
 	// Allocating palette data buffer
 	void *texture_palette = gpu_alloc_mapped_aligned(64, 256 * sizeof(uint32_t), use_vram ? VGL_MEM_VRAM : VGL_MEM_RAM);
--- source/utils/gpu_utils.h
+++ source/utils/gpu_utils.h
@@ -65,6 +65,35 @@ typedef struct {
 #endif
 } texture;
 
+static inline __attribute__((always_inline)) void gpu_store_texture_data(uint32_t orig_w, uint32_t w, uint32_t h, uint32_t src_stride, const void *src_data, void *dst_data, uint8_t src_bpp, uint8_t bpp, uint32_t (*read_cb)(void *), void (*write_cb)(void *, uint32_t), GLboolean fast_store, GLint xoffset) {
+	uint32_t dst_stride = ALIGN(orig_w, 8) * bpp;
+	uint8_t *src;
+	uint8_t *dst;
+	int i, j;
+	if (fast_store) { // Internal Format and Data Format are the same, we can just use vgl_fast_memcpy for better performance
+		if (xoffset == 0 && src_stride == dst_stride && src_stride == orig_w * bpp) // Texture size is already aligned, we can use a single vgl_fast_memcpy for better performance
+			sceClibMemcpy(dst_data, src_data, w * h * bpp);
+		else {
+			for (i = 0; i < h; i++) {
+				src = (uint8_t *)src_data + src_stride * i;
+				dst = (uint8_t *)dst_data + dst_stride * i;
+				sceClibMemcpy(dst, src, w * bpp);
+			}
+		}
+	} else { // Different internal and data formats, we need to go with slower callbacks system
+		for (i = 0; i < h; i++) {
+			src = (uint8_t *)src_data + src_stride * i;
+			dst = (uint8_t *)dst_data + dst_stride * i;
+			for (j = 0; j < w; j++) {
+				uint32_t clr = read_cb(src);
+				write_cb(dst, clr);
+				src += src_bpp;
+				dst += bpp;
+			}
+		}
+	}
+}
+
 // Alloc a generic memblock into sceGxm mapped memory
 void *gpu_alloc_mapped(size_t size, vglMemType type);
 
@@ -88,9 +117,6 @@ void gpu_fragment_usse_free_mapped(void *addr);
 
 // Calculate bpp for a requested texture format
 int tex_format_to_bytespp(SceGxmTextureFormat format);
-
-// Store texture data
-void gpu_store_texture_data(uint32_t orig_w, uint32_t w, uint32_t h, uint32_t src_stride, const void *src_data, void *dst_data, uint8_t src_bpp, uint8_t bpp, uint32_t (*read_cb)(void *), void (*write_cb)(void *, uint32_t), GLboolean fast_store, GLint xoffset);
 
 // Alloc a texture
 void gpu_alloc_texture(uint32_t w, uint32_t h, SceGxmTextureFormat format, const void *data, texture *tex, uint8_t src_bpp, uint32_t (*read_cb)(void *), void (*write_cb)(void *, uint32_t), GLboolean fast_store);
