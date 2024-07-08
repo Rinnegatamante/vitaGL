39b3e5f7bbd73f9ee4e329da0d4ef9946974de61
ZenoArrows <129334871+ZenoArrows@users.noreply.github.com>
2024-07-08 21:43:56 +0200

    Add value to gl error

--- source/textures.c
+++ source/textures.c
@@ -1475,7 +1475,7 @@ void _glCompressedTexImage2D(texture *tex, GLenum target, GLint level, GLenum in
 		if (planar_format) {
 #ifndef SKIP_ERROR_HANDLING
 			if (level > 0) {
-				SET_GL_ERROR(GL_INVALID_VALUE)
+				SET_GL_ERROR_WITH_VALUE(GL_INVALID_VALUE, level)
 			}
 #endif
 			gpu_alloc_yuv420p_texture(width, height, tex_format, data, tex);
