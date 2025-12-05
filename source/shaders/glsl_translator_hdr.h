937cd97d5e2680ce2d35ce9c9ae56c716cb2f173
Rinnegatamante <rinnegatamante@gmail.com>
2025-12-05 13:50:05 +0100

    Implement bias variant of texture2DProj in GLSL translator.

--- source/shaders/glsl_translator_hdr.h
+++ source/shaders/glsl_translator_hdr.h
@@ -73,8 +73,10 @@ inline int notEqual(float a, float b) { return (a != b ? 1 : 0); }
 inline int2 notEqual(float2 a, float2 b) { return int2(a.x != b.x ? 1 : 0, a.y != b.y ? 1 : 0); }
 inline int3 notEqual(float3 a, float3 b) { return int3(a.x != b.x ? 1 : 0, a.y != b.y ? 1 : 0, a.z != b.z ? 1 : 0); }
 inline int4 notEqual(float4 a, float4 b) { return int4(a.x != b.x ? 1 : 0, a.y != b.y ? 1 : 0, a.z != b.z ? 1 : 0, a.w != b.w ? 1 : 0); }
-inline float4 texture2DProj(sampler2D s, float3 c) { return tex2D(s, c); }
-inline float4 texture2DProj(sampler2D s, float4 c) { return tex2D(s, c.xyw); }
+inline float4 texture2DProj(sampler2D s, float3 c) { return tex2Dproj(s, c); }
+inline float4 texture2DProj(sampler2D s, float4 c) { return tex2Dproj(s, c.xyw); }
+inline float4 texture2DProj(sampler2D s, float4 c, float b) { return tex2Dbias(s, float4(c.xy / c.w, 1, b)); }
+inline float4 texture2DProj(sampler2D s, float3 c, float b) { return tex2Dbias(s, float4(c.xy / c.z, 1, b)); }
 #define inversesqrt rsqrt
 #define samplerCube samplerCUBE
 inline float4 glslTexture2D(sampler2D x, float2 s) { return tex2D(x,s); }
